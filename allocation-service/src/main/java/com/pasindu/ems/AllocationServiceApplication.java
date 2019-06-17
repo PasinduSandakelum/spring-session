500735bd98ac14d23b65bfa643c15504e22bfa5d
PasinduSandakelum <tmspthilakarathna2017@gmail.com>
2019-06-17 16:23:20 +0530

    config server and zuul updates

--- allocation-service/src/main/java/com/pasindu/ems/AllocationServiceApplication.java
+++ allocation-service/src/main/java/com/pasindu/ems/AllocationServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableResourceServer
 @EnableEurekaClient
-public class Application {
+public class AllocationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(AllocationServiceApplication.class, args);
 	}
 
 }
